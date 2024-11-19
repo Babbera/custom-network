be8fedb1010c401f247e58a8a19d608dbc8223a8
vaasu33551 <vaasu33551@gmail.com>
2024-11-19 16:10:52 +0530

    two

---
+++ variable.tf
@@ -0,0 +1,41 @@
+/*variable "vpc_id" {
+    type = string
+    default = ""
+  
+}
+variable "gateway_id" {
+    type = string
+    default = ""
+  
+}
+variable "route_table_id" {
+    type = string
+    default = ""
+  
+}
+variable "subnet_id" {
+    type = string
+    default = ""
+  
+}
+variable "vpc_security_group_ids" {
+    type = string
+    default = ""
+  
+}*/
+variable "ami" {
+    type = string
+    default = ""
+  
+}
+variable "key_name" {
+    type = string
+    default = ""
+}
+variable "instance_type" {
+    type = string
+    default = ""
+  
+}
+
+
